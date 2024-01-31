4af618c0679160f14d4ac99db0664637a6c2426d
pedro-donoso <pedro.donoso.r@protonmail.com>
2024-01-31 15:18:57 -0300

    creo tabla dinamica

--- .history/js/app_20240131151341.js
+++ .history/js/app_20240131151341.js
@@ -12,7 +12,7 @@ const cargarTabla = () => {
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
         <td>
-            <div class="d-flex gap-2">
+            <div>
             <button class="btn btn-outline-warning">
             <img src="img/boligrafo.png"/>
             </button>
